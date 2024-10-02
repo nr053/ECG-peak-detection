cf93944c4661a126b135d8da56ce9320d2963cb9
rose@rose <nr053@outlook.com>
2024-10-02 12:18:02 +0000

    use config file to load params

--- utils/db_generator.py
+++ utils/db_generator.py
@@ -1,8 +1,14 @@
 import numpy as np
 import torch.utils.data as data
 import pandas as pd
+import yaml
 
-feature_shape = 2048
+
+with open("config.yaml") as f:
+    cfg = yaml.load(f, Loader=yaml.FullLoader)
+
+feature_shape = cfg["feature_shape"]
+batch_size = cfg["batch_size"]
 
 class Test_Loader(data.Dataset):
     def __init__(self, set_dict):
@@ -23,6 +29,10 @@ class Test_Loader(data.Dataset):
             feature = np.pad(feature, ((0, padding), (0, 0)))
             target = np.pad(target, (0, padding))
 
+        #crop input and target for training purposes
+        #feature = feature[:2048]
+        #target = target[:2048]
+
         X = np.swapaxes(feature, 0, 1)
         y = target[np.newaxis, :]
 
@@ -39,13 +49,17 @@ class Train_Loader(data.Dataset):
         return self.iteration
 
     def __getitem__(self, index):
-        feature = self.list_feature.iloc[index]
-        target = self.list_target.iloc[index]
+        feature = self.list_feature[index]
+        target = self.list_target[index]
 
         padding = -feature.shape[0]%feature_shape # cropping
         if padding != 0:
             feature = np.pad(feature, ((0, padding), (0, 0)))
             target = np.pad(target, (0, padding))
+
+        #crop input and target for training purposes
+        feature = feature[:2048]
+        target = target[:2048]
 
         X = np.swapaxes(feature, 0, 1)
         y = target[np.newaxis, :]
@@ -54,27 +68,32 @@ class Train_Loader(data.Dataset):
 
 
 
-def Test_Generator(set_dict, shuffle):
+def Test_Generator(set_dict):
     data_loader = Test_Loader(set_dict)
-    data_generator = data.DataLoader(data_loader, batch_size=1)
+    data_generator = data.DataLoader(data_loader, batch_size=1, shuffle=False)
+    return data_generator
+
+def Validation_Generator(set_dict):
+    data_loader = Test_Loader(set_dict)
+    data_generator = data.DataLoader(data_loader, batch_size=128, shuffle=True)
     return data_generator
 
 def Train_Generator(set_dict):
     #shuffle the data
-    set_dict = pd.DataFrame.from_dict(set_dict).sample(frac=1, random_state=1)
+    #train_dict = pd.DataFrame.from_dict(set_dict)#.sample(frac=1, random_state=1)
     
     
-    train_size = int(len(set_dict)*0.8)
-    test_size = int((len(set_dict) - train_size)/2) #size of validation and test set
+    #train_size = int(len(set_dict)*0.8)
+    #test_size = int((len(set_dict) - train_size)/2) #size of validation and test set
 
-    train_dict = set_dict.iloc[:train_size].copy()
-    validation_dict = set_dict.iloc[train_size:train_size+test_size].copy()
-    test_dict = set_dict.iloc[train_size+test_size:].copy()
+    #train_dict = set_dict.iloc[:train_size].copy()
+    #validation_dict = set_dict.iloc[train_size:train_size+test_size].copy()
+    #test_dict = set_dict.iloc[train_size+test_size:].copy()
 
-    data_loader_train = Train_Loader(train_dict)
-    data_loader_test = Train_Loader(validation_dict)
+    data_loader_train = Train_Loader(set_dict)
+    #data_loader_test = Train_Loader(validation_dict)
     
-    data_generator_train = data.DataLoader(data_loader_train, batch_size=32, shuffle=True)
-    data_generator_test = data.DataLoader(data_loader_test, batch_size=32, shuffle=True)
+    data_generator_train = data.DataLoader(data_loader_train, batch_size=batch_size, shuffle=True)
+    #data_generator_test = data.DataLoader(data_loader_test, batch_size=32, shuffle=True)
     
-    return data_generator_train, data_generator_test, set_dict
+    return data_generator_train
