6f0a45eb6d9c7d5f453d022214f4aa6d759078ce
rose@rose <nr053@outlook.com>
2024-10-28 10:31:42 +0000

    added config

--- sandbox.py
+++ sandbox.py
@@ -14,8 +14,16 @@ import yaml
 import pywt
 from scipy import signal
 import matplotlib.pyplot as plt
+import random
+from sklearn.preprocessing import MinMaxScaler
 
 
+#config
+with open("config.yaml") as f:
+    cfg = yaml.load(f, Loader=yaml.FullLoader)
+
+feature_shape = cfg['feature_shape']
+target_lv = cfg['target_level']
 
 ### filtering method
 def dwt_idwt(array, wavelet='db3', level=9):
@@ -58,14 +66,16 @@ def normalization(array):
     return array_norm
 
 def sw_transform(array):
-    len_padding = -len(array)%2**4
+    #target_lv = 1 #for debug purposes
+    len_padding = -len(array)%2**target_lv
     padded = np.pad(array, (0, len_padding), 'edge')
-    coeff_swt = pywt.swt(padded, 'sym4', level=4, trim_approx=True)
+    coeff_swt = pywt.swt(padded, 'sym4', level=target_lv, trim_approx=True)
     # [cAn, cDn, ..., cD2, cD1]
     coeff_swt.reverse()
     # [cD1, cD2, ... ,cDn, cAn]
 
     feature = coeff_swt[3]
+    #feature = coeff_swt[1] #for debug purposes
     feature = feature[:len(array)]
     feature = normalization(feature)
 
@@ -93,238 +103,263 @@ def append_dicts(dict1, dict_list:[]):
     return dict1
 
 #vaf
-#with open("/home/rose/Cortrium/R-peak-detector/VAF_subset/test/00a4aad0-5d1b-4456-a6f0-8e5c13d0feda.pkl", 'rb') as file:
-#    vaf = pickle.load(file)
+with open("/home/rose/Cortrium/Databases/10K_VAF_subset/VAF_subset/train/00a4aad0-5d1b-4456-a6f0-8e5c13d0feda.pkl", 'rb') as file:
+    vaf = pickle.load(file)
 
 
-#edf
-#raw = mne.io.read_raw_edf("/home/rose/Cortrium/ECG-peak-detection/edf1.edf", preload=True)
-#data = raw.get_data()
+# #edf
+# #raw = mne.io.read_raw_edf("/home/rose/Cortrium/ECG-peak-detection/edf1.edf", preload=True)
+# #data = raw.get_data()
 
 
-# other files
-record = wfdb.rdsamp("/home/rose/Cortrium/Databases/MIT_BIH/100")
-ecg = record[0][:, 1]
-fs = record[1]['fs']
-# load annotation
-ann = wfdb.rdann("/home/rose/Cortrium/Databases/MIT_BIH/100", 'atr')
+# # other files
+# record = wfdb.rdsamp("/home/rose/Cortrium/Databases/MIT_BIH/100")
+# ecg = record[0][:, 1]
+# fs = record[1]['fs']
+# # load annotation
+# ann = wfdb.rdann("/home/rose/Cortrium/Databases/MIT_BIH/100", 'atr')
 
 
-# other files
-record_bw = wfdb.rdsamp("/home/rose/Cortrium/Databases/MIT_BIH_NST/bw")
-record_ma = wfdb.rdsamp("/home/rose/Cortrium/Databases/MIT_BIH_NST/ma")
-record_em = wfdb.rdsamp("/home/rose/Cortrium/Databases/MIT_BIH_NST/em")
-ecg_bw = np.swapaxes(record_bw[0],0,1)[0,:2520]
-ecg_ma = np.swapaxes(record_ma[0],0,1)[0,:2520]
-ecg_em = np.swapaxes(record_em[0],0,1)[0,:2520]
-
-data_train = DB_loading()
-dict_incart = data_train.create_set("INCART", train=True)
-dict_mit = data_train.create_set("MIT_BIH", train=True)
-dict_qt = data_train.create_set("QTDB", train=True)
-dict_train = append_dicts(dict_incart,  [dict_mit, dict_qt])
-dataloader_train = Train_Generator(dict_train)
-
-f1 = np.swapaxes(dict_train["feature"][500], 0, 1)[0]
-f2 = np.swapaxes(dict_train["feature"][500], 0, 1)[1]
-
-noise = np.random.normal(0,0.01,2520)
-noise_filtered = filtering(noise)
-noise_feature = transform(noise_filtered)
-noise_feature = np.swapaxes(noise_feature, 0, 1)
-noise_feature = noise_feature*0.1 #scale the noise
-
-x = np.arange(0, 3*np.pi, 0.1)
-x = signal.resample(x, 2520)
-sin = np.sin(x)
-
-ecg_bw_filtered = filtering(ecg_bw)
-ecg_ma_filtered = filtering(ecg_ma)
-ecg_em_filtered = filtering(ecg_em)
-
-ecg_bw_feature = transform(ecg_bw)
-ecg_ma_feature = transform(ecg_ma)
-ecg_em_feature = transform(ecg_em)
-#swap axis
-ecg_bw_feature = np.swapaxes(ecg_bw_feature, 0, 1)
-ecg_ma_feature = np.swapaxes(ecg_ma_feature, 0, 1)
-ecg_em_feature = np.swapaxes(ecg_em_feature, 0, 1)
-
-#feature 1
-plt.figure()
-plt.plot(f1)
-plt.savefig("f1.png")
-#feature 2
-plt.figure()
-plt.plot(f2)
-plt.savefig("f2.png")
-#noise
-plt.figure()
-plt.plot(noise)
-plt.savefig("noise.png")
-#noise filtered
-plt.figure()
-plt.plot(noise_filtered)
-plt.savefig("noise_filtered.png")
-#noise features
-plt.figure()
-plt.plot(noise_feature[0])
-plt.savefig("noise_feature1.png")
-plt.figure()
-plt.plot(noise_feature[1])
-plt.savefig("noise_feature2.png")
-#feature 1 + noise feature 1
-plt.figure()
-plt.plot(f1 + noise_feature[0])
-plt.savefig("f1_noisy.png")
-#feature 2 + noise feature 2
-plt.figure()
-plt.plot(f2 + noise_feature[1])
-plt.savefig("f2_noisy.png")
-#feature 1 augmented
-plt.figure()
-plt.plot((f1 + noise_feature[0])*sin)
-plt.savefig("f1_augmented.png")
-#feature 2 augmented
-plt.figure()
-plt.plot((f2 + noise_feature[1])*sin)
-plt.savefig("f2_augmented.png")
-
-#bassline wander
-plt.figure()
-plt.plot(ecg_bw)
-plt.savefig("bw.png")
-#bassline wander filtered
-plt.figure()
-plt.plot(ecg_bw_filtered)
-plt.savefig("bw_filtered.png")
-#bassline wander features
-plt.figure()
-plt.plot(ecg_bw_feature[0])
-plt.savefig("bw_feature1.png")
-plt.figure()
-plt.plot(ecg_bw_feature[1])
-plt.savefig("bw_feature2.png")
-
-#muscle artifact
-plt.figure()
-plt.plot(ecg_ma)
-plt.savefig("ma.png")
-#muscle artifact filtered
-plt.figure()
-plt.plot(ecg_ma_filtered)
-plt.savefig("ma_filtered.png")
-#muscle artifact features
-plt.figure()
-plt.plot(ecg_ma_feature[0])
-plt.savefig("ma_feature1.png")
-plt.figure()
-plt.plot(ecg_ma_feature[1])
-plt.savefig("ma_feature2.png")
-
-#electrode motion
-plt.figure()
-plt.plot(ecg_em)
-plt.savefig("em.png")
-#electrode motion filtered
-plt.figure()
-plt.plot(ecg_em_filtered)
-plt.savefig("em_filtered.png")
-#electrode motion features
-plt.figure()
-plt.plot(ecg_em_feature[0])
-plt.savefig("em_feature1.png")
-plt.figure()
-plt.plot(ecg_em_feature[1])
-plt.savefig("em_feature2.png")
-plt.close()
+# # artifical noise
+# record_bw = wfdb.rdsamp("/home/rose/Cortrium/Databases/MIT_BIH_NST/bw")
+# record_ma = wfdb.rdsamp("/home/rose/Cortrium/Databases/MIT_BIH_NST/ma")
+# record_em = wfdb.rdsamp("/home/rose/Cortrium/Databases/MIT_BIH_NST/em")
 
 
-#scale the artificial noise again
-ecg_bw_feature = ecg_bw_feature*abs(np.random.normal(0,0.05, 1))
-ecg_ma_feature = ecg_ma_feature*abs(np.random.normal(0,0.05, 1))
-ecg_em_feature = ecg_em_feature*abs(np.random.normal(0,0.05, 1))
+# bassline_wander = record_bw[0][:, 1] #use second channel only
+# muscle_artifact = record_ma[0][:, 1]
+# electrode_motion = record_em[0][:, 1]
 
+# #preprocess (filter + transform)
+# bassline_wander_filtered = filtering(bassline_wander)
+# muscle_artifact_filtered = filtering(muscle_artifact)
+# electrode_motion_filtered = filtering(electrode_motion)
 
-#plot the augmented waveforms
-#bassline wander
-plt.figure()
-plt.plot((f1 + noise_feature[0])*sin + ecg_bw_feature[0])
-plt.savefig("f1_aug_w_bw.png")
-plt.figure()
-plt.plot((f2 + noise_feature[1])*sin + ecg_bw_feature[1])
-plt.savefig("f2_aug_w_bw.png")
-#muscle artifact
-plt.figure()
-plt.plot((f1 + noise_feature[0])*sin + ecg_ma_feature[0])
-plt.savefig("f1_aug_w_ma.png")
-plt.figure()
-plt.plot((f2 + noise_feature[1])*sin + ecg_ma_feature[1])
-plt.savefig("f2_aug_w_ma.png")
-#electrode motion
-plt.figure()
-plt.plot((f1 + noise_feature[0])*sin + ecg_em_feature[0])
-plt.savefig("f1_aug_w_em.png")
-plt.figure()
-plt.plot((f2 + noise_feature[1])*sin + ecg_em_feature[1])
-plt.savefig("f2_aug_w_em.png")
-#all three
-plt.figure()
-plt.plot((f1 + noise_feature[0])*sin + ecg_bw_feature[0] + ecg_ma_feature[0] + ecg_em_feature[0])
-plt.savefig("f1_aug_w_everything.png")
-plt.figure()
-plt.plot((f2 + noise_feature[1])*sin + ecg_bw_feature[1] + ecg_ma_feature[1] + ecg_em_feature[1])
-plt.savefig("f2_aug_w_everything.png")
-
-# fig = go.Figure(
-#     data = [
-#         go.Scatter(y=np.swapaxes(ecg_bw, 0, 1)[1]),
-#         go.Scatter(y=np.swapaxes(ecg_bw, 0, 1)[0])
-#     ]
-# )
-# fig.write_image("foo.png")
-# fig.write_html("foo.html")
-
-
-# fig = px.line(x=ecg_bw)
-# fig.write_image("foo1.png")
-# fig.write_html("foo1.html")
-
-#usable_channels, _ = return_good_ecg_channel_idx_based_on_lead_off(vaf["strip_ecg"][0], vaf["lead_off"][0], 3)
-
-
-# fig = make_subplots(rows=5, cols=1)
-# #x_val = np.linspace(0,)
-# fig.add_trace(
-#     go.Scatter(y = vaf["strip_ecg"][0][0], name="ECG 1"),
-#     row=1, col=1,
-# )
-# fig.add_trace(
-#     go.Scatter(y = vaf["strip_ecg"][0][1], name="ECG 2"),
-#     row=2, col=1
-# )
-# fig.add_trace(
-#     go.Scatter(y = vaf["strip_ecg"][0][2], name="ECG 3"),
-#     row=3, col=1
-# )
-# fig.add_trace(
-#     go.Scatter(y = vaf["strip_ecg"][0][1], name="ECG 2"),
-#     row=4, col=1
-# )
-# fig.add_trace(
-#     go.Scatter(y = vaf["strip_ecg"][0][2], name="ECG 3"),
-#     row=5, col=1
-# )
-# fig.update_layout(height=600, title_text="YES THEN")
-# fig.write_image("foo.png")
-# fig.write_html("foo.html")
+# bassline_wander_transformed = transform(bassline_wander_filtered)
+# muscle_artifact_transformed = transform(muscle_artifact_filtered)
+# electrode_motion_transformed = transform(electrode_motion_filtered)
 
 
 
 
-# fig, axs = plt.subplots(3)
-# axs[0].plot(ecg[idx])
-# axs[1].plot(target[idx])
-# axs[1].vline()
-# axs[2].plot(pred[idx][:target[idx].shape[0]])
-# plt.savefig("foo.png")
+
+
+# data_train = DB_loading()
+# dict_incart = data_train.create_set("INCART", train=True)
+# dict_mit = data_train.create_set("MIT_BIH", train=True)
+# dict_qt = data_train.create_set("QTDB", train=True)
+# dict_train = append_dicts(dict_incart,  [dict_mit, dict_qt])
+# dataloader_train = Train_Generator(dict_train)
+
+# #take a random sample from the input space
+# input = dict_train["feature"][500][:feature_shape,:]
+
+# #plot the input before augmentation
+# fig, axs = plt.subplots(2)
+# fig.tight_layout()
+# axs[0].plot(np.swapaxes(input, 0, 1)[0])
+# axs[0].set_title("feature 1")
+# axs[1].plot(np.swapaxes(input, 0, 1)[1])
+# axs[1].set_title("feature 2")
+# fig.suptitle("input before augmentation")
+# plt.savefig("input_before_augmentation.png")
+# plt.close()
+
+
+# #generate white noise
+# noise = np.random.normal(0,1,2048)
+# noise_filtered = filtering(noise)
+# noise_feature = transform(noise_filtered, use_swt=True)
+# noise_feature_scaled = noise_feature*0.1 #scale the noise
+
+# #plot white noise
+# fig, axs = plt.subplots(4)
+# fig.tight_layout()
+# axs[0].plot(noise)
+# axs[0].set_title("noise")
+# axs[1].plot(noise_filtered)
+# axs[1].set_title("filtered")
+# axs[2].plot(noise_feature)
+# axs[2].set_title("transformed")
+# axs[3].plot(noise_feature_scaled)
+# axs[3].set_title("scaled")
+# fig.suptitle("White noise")
+# plt.savefig("white_noise.png")
+# plt.close()
+
+
+# #generate sin wave
+# x = np.arange(0, 3*np.pi, 0.1)
+# sin = np.sin(x)
+# sin = signal.resample(sin, 2048)
+# sin_scaled = sin*random.uniform(0.5,1.5)
+
+# #plot sin wave
+# fig, axs = plt.subplots(2)
+# fig.tight_layout()
+# axs[0].plot(sin)
+# axs[0].set_title("sin")
+# axs[1].plot(sin_scaled)
+# axs[1].set_title("sin scaled")
+# fig.suptitle("sin wave")
+# plt.savefig("sin.png")
+# plt.close()
+
+# #generate artificial noise
+# #choose random snippet from each type
+# start_idx_bw = random.randint(0,bassline_wander_transformed.shape[0] - feature_shape)
+# start_idx_ma = random.randint(0,muscle_artifact_transformed.shape[0] - feature_shape)
+# start_idx_em = random.randint(0,electrode_motion_transformed.shape[0] - feature_shape)
+# bassline_wander_sample = bassline_wander_transformed[start_idx_bw:start_idx_bw+feature_shape]
+# muscle_artifact_sample = muscle_artifact_transformed[start_idx_ma:start_idx_ma+feature_shape]
+# electrode_motion_sample = electrode_motion_transformed[start_idx_em:start_idx_em+feature_shape]
+
+# # #scale 
+# # percentile_99 = np.quantile(input, 0.999)
+# # percentile_01 = np.quantile(input, 0.001)
+
+# # bassline_wander = bassline_wander.reshape(-1,1)
+# # muscle_artifact = muscle_artifact.reshape(-1,1)
+# # electrode_motion = electrode_motion.reshape(-1,1)
+
+# # scaler = MinMaxScaler(feature_range=(percentile_01, percentile_99))
+# # scaler.fit(bassline_wander)
+# # bassline_wander_scaled = scaler.transform(bassline_wander)
+# # scaler.fit(muscle_artifact)
+# # muscle_artifact_scaled = scaler.transform(muscle_artifact)
+# # scaler.fit(electrode_motion)
+# # electrode_motion_scaled = scaler.transform(electrode_motion)
+
+
+
+
+# #plot artificial noise
+# fig, axs = plt.subplots(4,3)
+# fig.tight_layout()
+# axs[0,0].plot(bassline_wander[start_idx_bw:start_idx_bw+feature_shape])
+# axs[0,0].set_title("bassline_wander")
+# #axs[1,0].plot(bassline_wander_scaled)
+# #axs[1,0].set_title("bassline_wander scaled")
+# axs[1,0].plot(bassline_wander_filtered[start_idx_bw:start_idx_bw+feature_shape])
+# axs[1,0].set_title("bassline_wander filtered")
+# axs[2,0].plot(bassline_wander_transformed[start_idx_bw:start_idx_bw+feature_shape,0])
+# axs[2,0].set_title("bassline_wander feature 1")
+# axs[3,0].plot(bassline_wander_transformed[start_idx_bw:start_idx_bw+feature_shape,1])
+# axs[3,0].set_title("bassline_wander feature 2")
+
+# axs[0,1].plot(muscle_artifact[start_idx_ma:start_idx_ma+feature_shape])
+# axs[0,1].set_title("muscle_artifact")
+# #axs[1,1].plot(muscle_artifact_scaled)
+# #axs[1,1].set_title("muscle_artifact scaled")
+# axs[1,1].plot(muscle_artifact_filtered[start_idx_ma:start_idx_ma+feature_shape])
+# axs[1,1].set_title("muscle_artifact filtered")
+# axs[2,1].plot(muscle_artifact_transformed[start_idx_ma:start_idx_ma+feature_shape,0])
+# axs[2,1].set_title("muscle_artifact feature 1")
+# axs[3,1].plot(muscle_artifact_transformed[start_idx_ma:start_idx_ma+feature_shape,1])
+# axs[3,1].set_title("muscle_artifact feature 2")
+
+# axs[0,2].plot(electrode_motion[start_idx_em:start_idx_em+feature_shape])
+# axs[0,2].set_title("electrode_motion")
+# #axs[1,2].plot(electrode_motion_scaled)
+# #axs[1,2].set_title("electrode_motion scaled")
+# axs[1,2].plot(electrode_motion_filtered[start_idx_em:start_idx_em+feature_shape])
+# axs[1,2].set_title("electrode_motion filtered")
+# axs[2,2].plot(electrode_motion_transformed[start_idx_em:start_idx_em+feature_shape,0])
+# axs[2,2].set_title("electrode_motion feature 1")
+# axs[3,2].plot(electrode_motion_transformed[start_idx_em:start_idx_em+feature_shape,1])
+# axs[3,2].set_title("electrode_motion feature 2")
+
+
+# fig.suptitle("artificial noise")
+# plt.savefig("artificial_noise.png")
+# plt.close()
+
+
+# #add artificial noise with 50% probability
+# artificial_noise = np.zeros(bassline_wander_sample.shape)
+# #if random.choice([True,False]):
+# artificial_noise_1 = artificial_noise + bassline_wander_sample
+# #if random.choice([True,False]):
+# artificial_noise_2 = artificial_noise_1 + muscle_artifact_sample
+# #if random.choice([True,False]):
+# artificial_noise_3 = artificial_noise_2 + electrode_motion_sample
+
+# #scale the mixed noise 
+# scale_factor = abs(np.random.normal(0,0.05))
+# if scale_factor < 2:
+#     artificial_noise_scaled = artificial_noise_3 * abs(np.random.normal(0,0.05))
+
+# fig, axs = plt.subplots(5,2)
+# fig.tight_layout()
+# axs[0,0].plot(artificial_noise)
+# axs[0,0].set_title("artificial_noise")
+
+# axs[1,0].plot(artificial_noise_1[:,0])
+# axs[1,0].set_title("artificial_noise_1 feature 1")
+# axs[1,1].plot(artificial_noise_1[:,1])
+# axs[1,1].set_title("artificial_noise_1 feature 2")
+
+# axs[2,0].plot(artificial_noise_2[:,0])
+# axs[2,0].set_title("artificial_noise_2 feature 1")
+# axs[2,1].plot(artificial_noise_2[:,1])
+# axs[2,1].set_title("artificial_noise_2 feature 2")
+
+# axs[3,0].plot(artificial_noise_3[:,0])
+# axs[3,0].set_title("artificial_noise_3 feature 1")
+# axs[3,1].plot(artificial_noise_3[:,1])
+# axs[3,1].set_title("artificial_noise_3 feature 2")
+
+# axs[4,0].plot(artificial_noise_scaled[:,0])
+# axs[4,0].set_title("artificial_noise_scaled feature 1")
+# axs[4,1].plot(artificial_noise_scaled[:,1])
+# axs[4,1].set_title("artificial_noise_scaled feature 2")
+
+# fig.suptitle("mixed artificial noise")
+# plt.savefig("artificial_noise_mixed.png")
+# plt.close()
+
+
+# #step 1 (white noise and amplitude adjustment)
+# input_1 = input + noise_feature
+# input_1[:,0] = input[:,0] * sin 
+# input_1[:,1] = input[:,1] * sin 
+# #step 2 (artificial noise)
+# input_2 = input_1 + artificial_noise
+# #step 3 (flip or reverse with 50% probability)
+# #if random.choice([True,False]):
+# input_flipped = input_2*-1
+# #if random.choice([True,False]):
+# input_reversed = input_flipped[::-1]
+
+
+# fig, axs = plt.subplots(5,2)
+# fig.tight_layout()
+
+# axs[0,0].plot(input[:,0])
+# axs[0,0].set_title("input f1")
+# axs[0,1].plot(input[:,1])
+# axs[0,1].set_title("input f2")
+
+# axs[1,0].plot(input_1[:,0])
+# axs[1,0].set_title("input with white noise and scaled f1")
+# axs[1,1].plot(input_1[:,1])
+# axs[1,1].set_title("input with white noise and scaled f2")
+
+# axs[2,0].plot(input_2[:,0])
+# axs[2,0].set_title("with artificial noise f1")
+# axs[2,1].plot(input_2[:,1])
+# axs[2,1].set_title("with artificial noise f2")
+
+# axs[3,0].plot(input_flipped[:,0])
+# axs[3,0].set_title("input_flipped f1")
+# axs[3,1].plot(input_flipped[:,1])
+# axs[3,1].set_title("input_flipped f2")
+
+# axs[4,0].plot(input_reversed[:,0])
+# axs[4,0].set_title("input_reversed f1")
+# axs[4,1].plot(input_reversed[:,1])
+# axs[4,1].set_title("input_reversed f2")
+
+# fig.suptitle("input features with augmentation")
+# plt.savefig("input_with_augmentation.png")
+# plt.close()
